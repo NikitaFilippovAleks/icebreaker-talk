f53424746bf4e4e638ff950ffa1817c3431e8099
Nikita Filippov <nikita.filippov.aleks@mail.ru>
2025-03-02 19:03:01 +0300

    1 - Collections edit

--- backend/app/controllers/api/v1/collections_controller.rb
+++ backend/app/controllers/api/v1/collections_controller.rb
@@ -3,6 +3,8 @@
 module Api
   module V1
     class CollectionsController < ApplicationController
+      before_action :find_collection, only: %i[show]
+
       # Request for collections list
       #
       # @return [Hash] JSON
@@ -23,6 +25,36 @@ module Api
         @collections = Collection.all
 
         render json: json_presented(@collections, 'collections', 'main')
+      end
+
+      def show
+        render json: json_presented(@collection, 'collection', 'detailed')
+      end
+
+      def create
+        result = Collection.create(collection_params)
+
+        status, presenter_name =
+          if result.valid?
+            [:ok, 'detailed']
+          else
+            [:unprocessable_entity, 'data_errors']
+          end
+
+        render json: json_presented(result, 'collection', presenter_name, errors: result.errors),
+               status:
+      end
+
+      def find_collection
+        @collection = Collection.find_by(id: params[:id])
+        not_found unless @collection
+      end
+
+      def collection_params
+        params.require(:collection).permit(
+          :name, :color, :description,
+          questions: [[:text]]
+        )
       end
     end
   end
--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -2,4 +2,15 @@
 
 class ApplicationController < ActionController::API
   include ApplicationHelper
+
+  # Выводим сообщение об ошибке, если запись не найдена
+  #
+  # @return [nil]
+  #
+  # @example not_found
+  #
+  # @example_return { "error": "Запись с таким id не найдена." }
+  def not_found
+    render json: { errors: ['Запись с таким id не найдена.'] }, status: :not_found
+  end
 end
